810c7d902e66d251274b260ca3cbaadb96bcf71c
John <johnsief12@gmail.com>
2023-04-24 17:38:24 -0400

    set up arithmetic functions

---
+++ script.js
@@ -0,0 +1,21 @@
+const add =  (a, b) => {
+    return a + b;
+  };
+  console.log(add(5,5))
+
+  const subtract = (a, b) => {
+    return a - b;
+  };
+  console.log(subtract(8,4))
+
+  const multiply = (a, b) => {
+    return a * b;
+  };
+
+  console.log(multiply(10,7))
+
+
+  const divide =  (a, b) => {
+    return a / b;
+  };
+  console.log(subtract(12,4))
