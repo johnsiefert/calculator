ee4a88dbdbb07108f86ae1cb8aac991426091886
John <johnsief12@gmail.com>
2023-05-01 17:47:40 -0400

    click del works deleting previous character

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const operator = '';
 const button = document.getElementById("buttons");
 const display = document.getElementById("display");
 const btn = document.querySelector('.btn')
+const operation = document.querySelector('.operator').value
+// const del = document.getElementById("del");
 // let firstOperator = null;
 // let secondOperator = null;
 // let result = null;
@@ -29,21 +31,29 @@ const operate = (operator, number1, number2) =>{
   switch (operator) {
     case '+':
        return add(number1, number2);
+       break;
     case '-':
        return subtract(number1, number2);
+       break;
     case '*':
       return  multiply(number1, number2);
+      break;
     case '/':
       return divide(number1, number2);
+      break;
        default:
         console.log("Error!")
   }
 };
 
-button.addEventListener("click", (e) =>{
-console.log(display.textContent += e.target.value)
-if(e.target.value === "clear") {
-  return display.textContent = ''
+button.addEventListener("click", (e) => {
+  let keyPress = display.textContent += e.target.value
+if(e.target.id === "clear") {
+   display.textContent = "";
+}else if(e.target.id === "del") {
+  display.textContent = display.textContent.slice(0, -1);
+}else {
+  keyPress
 }
 });
 
