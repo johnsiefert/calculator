deff5cdd82fa9612576d1995c752f3fef73a85ee
John <johnsief12@gmail.com>
2023-05-07 14:39:37 -0400

    revamped all click events

--- script.js
+++ script.js
@@ -1,59 +1,90 @@
-const number1 = '';
-const number2 = '';
-const operator = '';
-const button = document.getElementById("buttons");
-const display = document.getElementById("display");
-const btn = document.querySelector('.btn')
-const operation = document.querySelector('.operator').value
-// const del = document.getElementById("del");
-// let firstOperator = null;
-// let secondOperator = null;
-// let result = null;
+const btn = document.querySelectorAll(".btn");
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector(".equal");
+const currentOperand = document.querySelector('.current-operand');
+const previousOperand = document.querySelector('.previous-operand');
+
+currentOperand.textContent = ' ';
+previousOperand.textContent = ' ';
 
 const add = (number1, number2) => {
-  return parseFloat(number1) + parseFloat(number2);
+  return number1 + number2;
 };
 
 const subtract = (number1, number2) => {
-  return parseFloat(number1) - parseFloat(number2);
+  return number1 - number2;
 };
 
 const multiply = (number1, number2)=> {
-  return parseFloat(number1) * parseFloat(number2);
+  return number1 * number2;
 };
 
 const divide = (number1, number2)=> {
-  return parseFloat(number1) / parseFloat(number2);
+  return number1 / number2;
 };
 
 
-const operate = (operator, number1, number2) =>{
+const operate = (number1, number2, operator) =>{
   switch (operator) {
-    case '+':
-       return add(number1, number2);
-       break;
-    case '-':
-       return subtract(number1, number2);
-       break;
-    case '*':
-      return  multiply(number1, number2);
-      break;
-    case '/':
-      return divide(number1, number2);
-      break;
+    case "+":
+        add(number1, number2);
+        break;
+    case "-":
+      subtract(number1, number2);
+     break;
+    case "*":
+      multiply(number1, number2);
+      break
+    case "/":
+        divide(number1, number2);
+        break;
        default:
         console.log("Error!")
   }
+
 };
 
-button.addEventListener("click", (e) => {
-  let keyPress = display.textContent += e.target.value
-if(e.target.id === "clear") {
-   display.textContent = "";
-}else if(e.target.id === "del") {
-  display.textContent = display.textContent.slice(0, -1);
-}else {
-  keyPress
-}
+
+let storedNumber = '';
+let clickedOperator = ''
+let firstNumber = '';
+let result = '';
+currentOperand.textContent = 0;
+
+
+btn.forEach((number) => {
+  number.addEventListener('click', () => {
+    storedNumber += number.value;
+    currentOperand.textContent = storedNumber;
+  })
 });
+
+operators.forEach((operator => {
+  operator.addEventListener('click', () => {
+    if (firstNumber && storedNumber) {
+      displayResult();
+    }
+    // save the first number
+    firstNumber = storedNumber;
+    // get the operator that was clicked
+    clickedOperator = operator.textContent;
+    previousOperand.textContent = storedNumber + clickedOperator;
+    storedNumber = '';
+    console.log('FirstNumber' + firstNumber + 'Stored' + storedNumber)
+    console.log(clickedOperator);
+  })
+}));
+
+equal.addEventListener('click', () =>{
+  displayResult();
+});
+
+const  displayResult= () => {
+  result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator)
+  // update content of current operation with result and previous operand with the calculation, make storedNumber = result
+  currentOperand.textContent = result;
+  previousOperand.textContent = firstNumber + ' ' + clickedOperator + ' ' + storedNumber;
+  storedNumber = result;
+  console.log('FirstNumber' + firstNumber + 'Stored' + storedNumber);
+}
 
