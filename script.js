7ddfc7b26e2b9fe1e6a07ec246bcd7e19c4d816b
John <johnsief12@gmail.com>
2023-05-15 18:03:50 -0400

    revamped all functions again apparents to finally be working!

--- script.js
+++ script.js
@@ -1,90 +1,96 @@
-const btn = document.querySelectorAll(".btn");
+const numbers = document.querySelectorAll('.btn');
+const display = document.querySelector('#display');
 const operators = document.querySelectorAll('.operator');
-const equal = document.querySelector(".equal");
-const currentOperand = document.querySelector('.current-operand');
-const previousOperand = document.querySelector('.previous-operand');
+const clear = document.querySelector('#clear');
+const decimal = document.querySelector('#decimal');
+const equal = document.querySelector('#equal')
 
-currentOperand.textContent = ' ';
-previousOperand.textContent = ' ';
-
-const add = (number1, number2) => {
-  return number1 + number2;
-};
-
-const subtract = (number1, number2) => {
-  return number1 - number2;
-};
-
-const multiply = (number1, number2)=> {
-  return number1 * number2;
-};
-
-const divide = (number1, number2)=> {
-  return number1 / number2;
-};
+let displayValue = "";
+let currentNumber = "";
+let prevOperator = "";
 
 
-const operate = (number1, number2, operator) =>{
-  switch (operator) {
-    case "+":
-        add(number1, number2);
-        break;
-    case "-":
-      subtract(number1, number2);
-     break;
-    case "*":
-      multiply(number1, number2);
-      break
-    case "/":
-        divide(number1, number2);
-        break;
-       default:
-        console.log("Error!")
+
+// performs calculations and displays them
+function operate(operator, a, b){
+  switch (operator){
+    case "add" :
+      display.textContent = a + b
+      break;
+    case "subtract" :
+      display.textContent = a - b
+      break;
+    case "multiply" :
+      display.textContent = a * b
+      break;
+    case "divide" :
+      display.textContent = a / b
+      break;
   }
 
-};
-
-
-let storedNumber = '';
-let clickedOperator = ''
-let firstNumber = '';
-let result = '';
-currentOperand.textContent = 0;
-
-
-btn.forEach((number) => {
-  number.addEventListener('click', () => {
-    storedNumber += number.value;
-    currentOperand.textContent = storedNumber;
-  })
-});
-
-operators.forEach((operator => {
-  operator.addEventListener('click', () => {
-    if (firstNumber && storedNumber) {
-      displayResult();
-    }
-    // save the first number
-    firstNumber = storedNumber;
-    // get the operator that was clicked
-    clickedOperator = operator.textContent;
-    previousOperand.textContent = storedNumber + clickedOperator;
-    storedNumber = '';
-    console.log('FirstNumber' + firstNumber + 'Stored' + storedNumber)
-    console.log(clickedOperator);
-  })
-}));
-
-equal.addEventListener('click', () =>{
-  displayResult();
-});
-
-const  displayResult= () => {
-  result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator)
-  // update content of current operation with result and previous operand with the calculation, make storedNumber = result
-  currentOperand.textContent = result;
-  previousOperand.textContent = firstNumber + ' ' + clickedOperator + ' ' + storedNumber;
-  storedNumber = result;
-  console.log('FirstNumber' + firstNumber + 'Stored' + storedNumber);
+// stores current number and resets other
+  currentNumber = display.textContent;
+  prevOperator = "";
+  displayValue = "";
 }
+// adds events onto each number, when clicked called populateDisplay
+numbers.forEach(button => {
+  button.addEventListener('click', populateDisplay)
+});
+
+// populates display with the button press, adds additional presses to displayValue
+function populateDisplay() {
+  document.querySelector('#clear').innerHTML = "C";
+  (!displayValue) ? (displayValue = this.textContent) : (displayValue += this.textContent)
+  display.textContent = displayValue;
+}
+
+operators.forEach(button => {
+  button.addEventListener('click', () => {
+    // if a number and operator is stored operate
+    if(currentNumber && prevOperator){
+      operate(prevOperator, Number(currentNumber), Number(displayValue));
+
+    }
+    // if two numbers are stored without an operator store the number and reset the previous
+    else if (!prevOperator && currentNumber && displayValue){
+      currentNumber = displayValue;
+      displayValue = "";
+    }
+    // if current number exists reset display for the next number
+    else if (currentNumber){
+      displayValue = "";
+    }
+    else {
+    // if no values are stored, store displayValue to current number, reset display value
+      currentNumber = Number(displayValue)
+      displayValue = "";
+    }
+    // stores operator based on button press
+    prevOperator = button.id
+  });
+});
+
+// call the operate function
+equal.addEventListener('click', () => {
+  operate(prevOperator, Number(currentNumber), Number(displayValue));
+})
+
+// if there is no display value, displayValue = ".", if there is a display value add "." to it
+// store the result
+decimal.addEventListener('click', () => {
+  if(displayValue.toString().indexOf('.') === -1){
+    (!displayValue) ? (displayValue = ".") : (displayValue += ".")
+      }
+    display.textContent = displayValue
+    });
+
+// clear all values and operators
+clear.addEventListener('click', () =>{
+  display.innerText = 0;
+  displayValue = "";
+  currentNumber = "";
+  prevOperator = "";
+  document.querySelector('#clear').innerHTML = "C";
+});
 
